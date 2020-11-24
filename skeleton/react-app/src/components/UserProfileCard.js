7f2a3523c25602e341d06e7d03268c601f40d579
eric <lydaeric@gmail.com>
2020-11-23 19:52:16 -0500

    Finishing up for the day

--- skeleton/react-app/src/components/UserProfileCard.js
+++ skeleton/react-app/src/components/UserProfileCard.js
@@ -1,32 +1,32 @@
 import React, { useEffect, useState } from 'react'
+import { Avatar } from '@material-ui/core'
 
 
 function UserProfileCard() {
     const [user, setUser] = useState({});
-    const userId = 12
+    const userId = 2
     useEffect(() => {
         (async () => {
-            const response = await fetch(`/api/users/${userId}`);
+            const response = await fetch(`/api/users/`);
             const user = await response.json();
             console.log(user);
             setUser(user);
         })();
     }, [userId]);
 
-    console.log(user)
     if (!user.username) {
         return null
     }
 
-
     return (
         <div className="user-profile-card">
-            <p>{user.username}</p>
-            <p>{user.first_name}</p>
-            <p>{user.last_name}</p>
-            <p>{user.email}</p>
-            <p>{user.birthday}</p>
-            <p>{ user.stamp.stamp}</p>
+            <Avatar src={`/icons/${user.stamp.stamp}.svg`} />
+            <div className="user-details">
+                <div>{user.username}</div>
+                <div>{user.first_name} {user.last_name}</div>
+                <div>{user.email}</div>
+                <div>{user.birthday}</div>
+            </div>
         </div>
     )
 }
