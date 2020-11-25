6eb291505e3eb046d2467470b157f3bc5cf4b842
Brian Wang <brbwang@yahoo.com>
2020-11-24 20:32:22 -0500

    renderLinks function not working, rest rendering

--- skeleton/react-app/src/components/AboutCard.js
+++ skeleton/react-app/src/components/AboutCard.js
@@ -1,15 +1,41 @@
 import React from "react";
 
 function AboutCard(){
-  let links = [["GitHub", "https://www.github.com/"], ["LinkedIn", "https://www.linkedin.com/"]]
+  const renderLinks = (links) => {
+    console.log("links should be rendering")
+    console.log(links)
+    links.map(link => {
+      return <a key={links[1]} href={link[1]}>{link[0]}</a>
+    })
+  }
+  let users = [
+    {
+      name: "Brian",
+      email: "brbwang@gmail.com",
+      links: [["GitHub", "https://www.github.com/Awodfkai"], ["LinkedIn", "https://www.linkedin.com/"]],
+    },
+    {
+      name: "Alicia",
+      email: "otheremail@gmail.com",
+      links: [["GitHub", "https://www.github.com/"], ["LinkedIn", "https://www.linkedin.com/"]],
+    },
+  ]
   return (
-    <div className="AboutCardContainer">
-      <h3>Name</h3>
-      <p>email</p>
-      {links.map(link => {
-        return <a href={link[1]}>{link[0]}</a>
-      })}
-    </div>
+    <>
+      {
+        users.map(user => {
+          return (
+            <div key={user.name} className="AboutCardContainer">
+              <h3>{user.name}</h3>
+              <p>{user.email}</p>
+              <div>
+                {renderLinks(user.links)}
+              </div>
+            </div>
+          )
+        })
+      }
+    </>
   )
 }
 
