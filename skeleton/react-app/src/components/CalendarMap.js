0ea05c8121124007b8cb67f149c78cdb7109ac5e
David <dyclee@umich.edu>
2020-11-28 23:38:04 -0600

    editing date

--- skeleton/react-app/src/components/CalendarMap.js
+++ skeleton/react-app/src/components/CalendarMap.js
@@ -5,26 +5,25 @@ import 'react-calendar-heatmap/dist/styles.css';
 import '../styles/layouts.css'
 
 export default function CalendarMap () {
-    const [calendarData, setCalendarData] = useState()
+    const [calendarData, setCalendarData] = useState({})
     const { hid, mid } = useParams()
     useEffect(() => {
         (async () => {
             let dataFetch = await fetch(`/api/habits/${hid}/calendar/${mid}`)
             const resObj = await dataFetch.json()
             console.log("CALENDER RESPONSE", resObj)
+            setCalendarData(resObj)
         })()
-    })
+    }, [])
+
+    if (!calendarData.startDate) return null;
+
     return (
         <div className='calendarContainer'>
             <CalendarHeatmap
-            startDate={new Date('2015-11-01')}
-            endDate={new Date('2016-04-01')}
-            values={[
-              { date: '2016-01-01' },
-              { date: '2016-01-22' },
-              { date: '2016-01-30' },
-              // ...and so on
-            ]}
+            startDate={new Date(calendarData.startDate)}
+            endDate={new Date(calendarData.endDate)}
+            values={calendarData.values}
             />
 
         </div>
