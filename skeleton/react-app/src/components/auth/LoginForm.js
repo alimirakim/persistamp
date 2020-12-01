35a1a0cf675ec91c443997ca3ef254c5cc13f6fa
David <dyclee@umich.edu>
2020-12-01 08:03:45 -0600

    working on splash

--- skeleton/react-app/src/components/auth/LoginForm.js
+++ skeleton/react-app/src/components/auth/LoginForm.js
@@ -1,12 +1,18 @@
 import React, { useState } from "react";
 import { Redirect } from "react-router-dom";
 import { login } from "../../services/auth";
+import { FormControl, TextField, Button } from '@material-ui/core';
+import { ActionOrCancelButtons } from '../FormInputs';
+import SignUpForm from './SignUpForm';
+import '../../styles/layouts.css'
+
 
 const LoginForm = ({ authenticated, setAuthenticated, setUser }) => {
   const [errors, setErrors] = useState([]);
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  
+  const [open, setOpen] = React.useState(true);
+
   const onLogin = async (e) => {
     e.preventDefault();
     const user = await login(email, password);
@@ -17,7 +23,8 @@ const LoginForm = ({ authenticated, setAuthenticated, setUser }) => {
       setErrors(user.errors);
     }
   };
-
+  const handleClickOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false)
   const updateEmail = (e) => setEmail(e.target.value);
 
   const updatePassword = (e) => setPassword(e.target.value);
@@ -25,35 +32,50 @@ const LoginForm = ({ authenticated, setAuthenticated, setUser }) => {
   if (authenticated) return <Redirect to="/" />
 
   return (<>
-    <h2>Login</h2>
-    <form onSubmit={onLogin}>
+    <SignUpForm open={open} setOpen={setOpen} handleClickOpen={handleClickOpen} handleClose={handleClose} authenticated={authenticated} setAuthenticated={setAuthenticated} setUser={setUser} />
+   <div className="loginContainer">
+     <div>
       <div>
         {errors.map((error) => (
-          <div>{error}</div>
+          <div className="loginHeader">{error}</div>
         ))}
       </div>
-      <div>
-        <label htmlFor="email">Email</label>
-        <input
-          name="email"
-          type="text"
-          placeholder="Email"
-          value={email}
-          onChange={updateEmail}
-        />
-      </div>
-      <div>
-        <label htmlFor="password">Password</label>
-        <input
-          name="password"
-          type="password"
-          placeholder="Password"
-          value={password}
-          onChange={updatePassword}
-        />
-        <button type="submit">Login</button>
-      </div>
-    </form>
+      <h2 className="loginHeader">Persistamp</h2>
+      <form onSubmit={onLogin}>
+          <TextField
+            autoFocus
+            defaultValue={email}
+            margin="dense"
+            id="email"
+            label="Email"
+            type="text"
+            fullWidth
+            onChange={updateEmail}
+            required
+          />
+        <TextField
+              autoFocus
+              defaultValue={password}
+              margin="dense"
+              id="password"
+              label="Password"
+              type="password"
+              fullWidth
+              onChange={updatePassword}
+              required
+            />
+          {/* <button type="submit">Login</button> */}
+          <Button onClick={onLogin} color="primary">
+            Login
+          </Button>
+          <Button onClick={handleClickOpen} color="primary">
+            Sign up
+          </Button>
+      </form>
+
+     </div>
+   </div>
+
   </>);
 };
 
