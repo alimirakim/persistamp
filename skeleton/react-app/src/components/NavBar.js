a18ecdb56a10cff2fe146bd1993a7e7daae279bc
Alicia Mira Kim <alicia.mira.kim@gmail.com>
2020-12-01 12:34:50 -0500

    Fixed turtle.

--- skeleton/react-app/src/components/NavBar.js
+++ skeleton/react-app/src/components/NavBar.js
@@ -5,6 +5,7 @@ import '../styles/layouts.css';
 import UserContext from '../context/UserContext';
 import UserSettings from './UserSettings'
 import { logout } from "../services/auth";
+import turtle from '../images/turtle.svg'
 
 export default function NavBar({ authenticated, setAuthenticated, user }) {
   // const [isLoggedIn, setIsLoggedIn] = useState(authenticated)
@@ -22,9 +23,7 @@ export default function NavBar({ authenticated, setAuthenticated, user }) {
     return (<>
       <nav className="stickers">
         <NavLink to='/' className="sticker sticker_logo-link" activeClassName="active">
-          <img className="sticker_logo" src="/icons/turtle.svg" alt="Persistamp Logo: a red turtle stamp" />
-          <br />
-          <br />
+          <img className="sticker_logo" src={turtle} alt="Persistamp Logo: a red turtle stamp" />
         </NavLink>
         <Link to="/" className="sticker sticker_home">
         <i className="fas fa-home"></i>
@@ -54,7 +53,7 @@ export default function NavBar({ authenticated, setAuthenticated, user }) {
     return (<>
       <nav className="stickers">
         <NavLink to='/login' className="sticker sticker_logo-link" activeClassName="active">
-          <img className="sticker_logo" src="/icons/turtle.svg" alt="Persistamp Logo: a red turtle stamp" />
+          <img className="sticker_logo" src={turtle} alt="Persistamp Logo: a red turtle stamp" />
         </NavLink>
       </nav>
     </>)
