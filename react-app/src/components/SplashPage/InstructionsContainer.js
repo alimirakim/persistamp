ccf178a39c2fc0aa08a83083f5e2a529e476c843
David <dyclee@umich.edu>
2021-01-11 21:41:49 -0600

    working on splash

--- react-app/src/components/SplashPage/InstructionsContainer.js
+++ react-app/src/components/SplashPage/InstructionsContainer.js
@@ -1,19 +1,24 @@
 import React from 'react';
 import {InstructionBox} from './InstructionBox';
 
-import instruction1 from '../../images/persistamp-instruction1-resize.PNG';
-
+import instruction1 from '../../images/persistamp-instruction1.PNG';
+import instruction2 from '../../images/persistamp-instruction2.PNG';
+// import instruction3 from '../../images/persistamp-instruction3-resize.PNG';
 const instructions = [
     {
         number: 1,
-        text: "Create a program and habits you want to track.",
+        text: "Create programs and habits that you want to commit to.",
         image: instruction1
     },
     {
-        number: 2
+        number: 2,
+        text: "Customize and keep track of each habit.",
+        image: instruction2
     },
     {
-        number: 3
+        number: 3,
+        text: "Follow how you're doing and reward yourself.",
+        // image: instruction3
     },
 ]
 
