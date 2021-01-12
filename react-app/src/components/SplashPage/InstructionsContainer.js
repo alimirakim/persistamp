a691d1bd408d1103906cc062d7008486df0ff8da
David <dyclee@umich.edu>
2021-01-11 21:52:01 -0600

    images for splash

--- react-app/src/components/SplashPage/InstructionsContainer.js
+++ react-app/src/components/SplashPage/InstructionsContainer.js
@@ -3,7 +3,7 @@ import {InstructionBox} from './InstructionBox';
 
 import instruction1 from '../../images/persistamp-instruction1.PNG';
 import instruction2 from '../../images/persistamp-instruction2.PNG';
-// import instruction3 from '../../images/persistamp-instruction3-resize.PNG';
+import instruction3 from '../../images/persistamp-instruction3.PNG';
 const instructions = [
     {
         number: 1,
@@ -18,7 +18,7 @@ const instructions = [
     {
         number: 3,
         text: "Follow how you're doing and reward yourself.",
-        // image: instruction3
+        image: instruction3
     },
 ]
 
