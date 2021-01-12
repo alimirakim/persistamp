05e0876f905816876b0a6ef541ff2dad18c630ce
David <dyclee@umich.edu>
2021-01-11 22:50:52 -0600

    styling

--- react-app/src/components/SplashPage/InstructionBox.js
+++ react-app/src/components/SplashPage/InstructionBox.js
@@ -5,6 +5,12 @@ export const InstructionBox = ({obj}) => {
         <div className={`instruction-${obj.number}-numarrow`}>
         </div>
         <div className={`instruction-${obj.number}-text`}>
+            <div className="instructionContent">
+                <img className="numberIcons" src={obj.numberIcon} />
+                <div className='instructionText'>
+                    {obj.text}
+                </div>
+            </div>
         </div>
         <div className={`instruction-${obj.number}-image`}>
             <img className="instruction-screenshot" src={obj.image} alt="instruction pic" />
--- react-app/src/components/SplashPage/InstructionsContainer.js
+++ react-app/src/components/SplashPage/InstructionsContainer.js
@@ -4,32 +4,42 @@ import {InstructionBox} from './InstructionBox';
 import instruction1 from '../../images/persistamp-instruction1.PNG';
 import instruction2 from '../../images/persistamp-instruction2.PNG';
 import instruction3 from '../../images/persistamp-instruction3.PNG';
+import instruction4 from '../../images/persistamp-instruction4.PNG';
+
+import numberOne from '../../images/number1.png';
+import numberTwo from '../../images/number2.png';
+import numberThree from '../../images/number3.png';
+
 const instructions = [
     {
         number: 1,
         text: "Create programs and habits that you want to commit to.",
-        image: instruction1
+        image: instruction1,
+        numberIcon: numberOne,
     },
     {
         number: 2,
         text: "Customize and keep track of each habit.",
-        image: instruction2
+        image: instruction2,
+        numberIcon: numberTwo,
     },
     {
         number: 3,
         text: "Follow how you're doing and reward yourself.",
-        image: instruction3
+        image: instruction3,
+        numberIcon: numberThree,
     },
 ]
 
 export default function InstructionsContainer () {
     return (<>
-        {/* <div className="instructionscontainer"> */}
             {instructions.map((obj) => {
                 return (<>
                     <InstructionBox  obj={obj} />
                 </>)
             })}
-        {/* </div> */}
+            <div className={`instruction-4-image`}>
+                <img className="instruction-screenshot" src={instruction4} alt="instruction pic" />
+            </div>
     </>)
 }
