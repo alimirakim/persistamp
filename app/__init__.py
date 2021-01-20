2e8dc0ac4199ba3b5755a032b2775c725e553b88
David <dyclee@umich.edu>
2021-01-19 21:56:13 -0600

    added http/https function

--- app/__init__.py
+++ app/__init__.py
@@ -62,6 +62,18 @@ Migrate(app, db)
 CORS(app)
 
 
+@app.before_request
+def redirect_https():
+    # print("REQUEST", request.url)
+    if os.environ.get("FLASK_ENV") == "production":
+        if request.headers.get('X-Forwarded-Proto') == 'http':
+            url = request.url.replace('http://', 'https://', 1)
+            if not url.endswith('/'):
+                url += '/'
+            code = 301
+            return redirect(url, code=code)
+
+
 @app.after_request
 def inject_csrf_token(response):
     response.set_cookie('cookies', "sugar pumpkin")
